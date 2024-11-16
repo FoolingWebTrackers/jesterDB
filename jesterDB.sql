2b8a30b9b971b1b8576fdeaa4aadb118528e5091
BSAHLAN <91738720+BSAHLAN@users.noreply.github.com>
2024-11-16 18:06:42 +0300

    Added extra constraints to the database

--- jesterDB.sql
+++ jesterDB.sql
@@ -1,24 +1,24 @@
 CREATE TABLE users (
-    id INT PRIMARY KEY,
-    username TEXT,
-    password TEXT,
-    salt TEXT
+    id SERIAL PRIMARY KEY, 
+    username TEXT UNIQUE NOT NULL, 
+    password TEXT NOT NULL,
+    salt TEXT NOT NULL
 );
 
 CREATE TABLE persona (
-    id INT PRIMARY KEY,
-    name TEXT,
+    id SERIAL PRIMARY KEY, 
+    name TEXT NOT NULL,
     description TEXT,
     image BYTEA
 );
 
 CREATE TABLE user_personas (
-    user_id INT,
-    persona_id INT,
+    user_id INT NOT NULL,
+    persona_id INT NOT NULL,
     is_favorite BOOL,
     first_access_date DATE,
     PRIMARY KEY (user_id, persona_id),
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (persona_id) REFERENCES persona(id)
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (persona_id) REFERENCES persona(id) ON DELETE CASCADE
 );
 
